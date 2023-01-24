125233a4afbe75c5b21d85aad7e46675b4cb570f
Francisco Orozco ActiveIT-ThinkPad <francisco.s.orozco@outlook.com>
2023-01-24 14:50:30 +0000

    No detecta los asteriscos en el documento de prueb

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import docx
 
 # Abrir el archivo de Word
-doc = docx.Document('prueba.docx')
+doc = docx.Document('prueba3.docx')
 
 # Recorrer todas las oraciones del documento
 for para in doc.paragraphs:
     # Recorrer todas las palabras de cada oración
-    for run in para.runs:
+     for run in para.runs:
         # Reemplazar el símbolo * con una imagen
         if '*' in run.text:
             run.text = run.text.replace('*', '')
